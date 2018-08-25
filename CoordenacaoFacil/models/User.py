95ece015decd18c9a2815a3acf8ae4917eb5bd6d
eduardom <euodeionomedeusuario@gmail.com>
2018-08-25 03:59:08 -0300

    adding User.py

---
+++ CoordenacaoFacil/models/User.py
@@ -0,0 +1,10 @@
+
+class User():
+    def __init__(self, id=0, code=0, name="", email="", password="", type="", createdAt=""):
+        self.id = id
+        self.code = code
+        self.name = name
+        self.email = email
+        self.password = password
+        self.type = type
+        self.createdAt = createdAt
