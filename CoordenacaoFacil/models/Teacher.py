645f687dc1814cce489521877e0a8b0da8d4a7b0
eduardom <euodeionomedeusuario@gmail.com>
2018-08-26 12:27:10 -0300

    implementing create and get all teacers

--- CoordenacaoFacil/models/Teacher.py
+++ CoordenacaoFacil/models/Teacher.py
@@ -2,6 +2,22 @@ from User import User
 
 class Teacher(User):
     def __init__(self, id=0, code=0, name="", email="", password="", type="", createdAt="", course=None, university=None):
-        super.__init__(id=0, code=0, name="", email="", password="", type="", createdAt="")
+        super().__init__(id=id, code=code, name=name, email=email, password=password, type=type, createdAt=createdAt)
         self.course = course
         self.university = university
+
+
+    def createTeacher(self, teacher=None):
+        db.users.insert({
+            "name": teacher.name,
+            "code": teacher.code,
+            "course": teacher.course,
+            "type": "teacher"
+        })
+
+        return True
+
+    def getAllTeachers(self):
+        teachers = db.users.find({})
+
+        return teachers
